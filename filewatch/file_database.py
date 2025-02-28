41e1edc185d91dc74642599f349f51135856c406
Sean Warlick <warlick.sean@gmail.com>
2025-02-27 18:55:58 -0800

    fixed insert data and wrote new queries.

--- filewatch/file_database.py
+++ filewatch/file_database.py
@@ -46,22 +46,48 @@ class FileWatcherDatabase:
         except Error as e:
             print(e)
 
-    def __insert_data_sql(self):
-        """Inserts a new record into the file_events table """
+    # I inadvertently just set the expectation that all time conversions will be done before
+    # this function is called. I think this is a good idea. It means that the time conversion
+    # is not tied to the database.
+    def insert_data(
+        self,
+        event_time: int,
+        event_type: str,
+        event_location: str,
+        file_type: str,
+        move_destination: str = None,
+    ):
+        """
+        Inserts data into the database.
+        Args:
+            event_time (int): The time of the event. The time should be in integer form
+                (e.g., epoch time).
+            event_type (str): The type of the event.
+            event_location (str): The location of the event.
+            file_type (str): The type of the file.
+            move_destination (str, Optional): The destination where the file is moved. Defaults to None.
+        Returns:
+            None
+        Raises:
+            sqlite3.Error: If there is an error with the database operation.
+        """
+
         if self.__conn is None:
-            print('Error: Database connection is not established')
+            print("Error: Database connection is not established")
             return
 
         try:
             c = self.__conn.cursor()
-            c.execute(self.__insert_data_sql(), (event_id, event_time, event_type, event_location, file_type, move_destinatino))
+            c.execute(
+                self.__insert_data_sql(),
+                (event_time, event_type, event_location, file_type, move_destination),
+            )
             self.__conn.commit()
         except sqlite3.Error as e:
-            print(f'Database error:{e}')
+            print(f"Database error:{e}")
 
-    def query_by_file_extension(self, ext_param: str):
+    def query_by_file_extension(self, ext_param: str) -> list:
         """Queries the database by file type.
-
 
         Args:
             ext_param (str): The file extension of interest.
@@ -70,9 +96,52 @@ class FileWatcherDatabase:
         c.execute(self.__file_extension_query(), (ext_param))
 
         rows = c.fetchall()
-        # TODO: replace this place holder with how we actually display this data.
-        for row in rows:
-            print(row)
+        return rows
+
+    def query_by_event_type(self, event_type: str) -> list:
+        """Queries the database by event type.
+
+        Args:
+            event_type (str): The event type of interest.
+        """
+        c = self.__conn.cursor()
+        c.execute(self.__event_type_query(), (event_type))
+
+        rows = c.fetchall()
+
+        return rows
+
+    def query_by_event_location(self, event_location: str) -> list:
+        """Queries the database by event location.
+
+        Args:
+            event_location (str): The event location of interest.
+        """
+        c = self.__conn.cursor()
+        c.execute(self.__event_location_query(), (f"{event_location}%"))
+
+        rows = c.fetchall()
+
+        return rows
+
+    def query_by_event_date(self, start_time: int, end_time: int) -> list:
+        """Queries the database by event date.
+
+        Args:
+            start_time (int): The start time of interest. Should be in integer form
+                (e.g., epoch time).
+            end_time (int): The end time of interest. Should be in integer form
+                (e.g., epoch time).
+        """
+        c = self.__conn.cursor()
+        c.execute(self.__event_date_query(), (start_time, end_time))
+
+        rows = c.fetchall()
+
+        return rows
+
+    # Queries are defined below. All queries are private methods that return the SQL as
+    # a string. THe SQL must be parameterized.
 
     def __create_table_sql(self) -> str:
         """Holds query string for creating table."""
@@ -84,18 +153,36 @@ class FileWatcherDatabase:
 	      "event_type"	TEXT NOT NULL,
 	      "event_location"	TEXT NOT NULL,
 	      "file_type"	INTEGER,
-	      "move destination"	TEXT,
+	      "move_destination"	TEXT,
 	      PRIMARY KEY("event_id" AUTOINCREMENT)
         );
         """
         return sql
+
+    def __insert_data_sql(self) -> str:
+        """returns SQL query for inserting data into the file_events table"""
+        return """
+        
+        INSERT INTO file_events (
+            event_time,
+            event_type,
+            event_location,
+            file_type,
+            move destination
+        )
+        VALUES (?, ?, ?, ?, ?)
+        """
 
     def __file_extension_query(self) -> str:
         """returns SQL query for selecting by file extension"""
         return """
         
         SELECT
-            *
+            event_time,
+            event_type,
+            event_location,
+            file_type,
+            move destination
 
         FROM  file_events
 
@@ -103,3 +190,57 @@ class FileWatcherDatabase:
             file_type = ?
 
         """
+
+    def __event_type_query(self) -> str:
+        """returns SQL query for selecting by action type"""
+        sql = """
+        SELECT
+            event_time,
+            event_type,
+            event_location,
+            file_type,
+            move destination
+
+        FROM  file_events
+
+        Where
+            event_type = ?
+        """
+
+        return sql
+
+    def __event_location_query(self) -> str:
+        """returns SQL query for selecting by event location"""
+        sql = """
+        
+        SELECT
+            event_time,
+            event_type,
+            event_location,
+            file_type,
+            move destination
+
+        FROM  file_events
+
+        Where
+            event_location like ?
+        """
+        return sql
+
+    def __event_date_query(self) -> str:
+        """returns SQL query for selecting by event date"""
+        sql = """
+        
+        SELECT
+            event_time,
+            event_type,
+            event_location,
+            file_type,
+            move destination
+
+        FROM  file_events
+
+        Where
+            event_time between ? and ?
+        """
+        return sql
