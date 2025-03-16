6d427c83176a2c69f0c613636e419ff9b4ad461a
Sean Warlick <warlick.sean@gmail.com>
2025-03-16 16:35:22 -0700

    updated db file so we can query while watching. fix requirements file.

--- filewatch/file_database.py
+++ filewatch/file_database.py
@@ -21,30 +21,29 @@ class FileWatcherDatabase:
         self.__database_location = os.path.join(
             os.path.abspath(database_directory), database_name
         )
-        self.__conn: Union[sqlite3.Connection, None] = None
 
-    @property
-    def conn(self):
-        return self.__conn
+    def create_database_connection(self) -> sqlite3.Connection:
+        """Creates a connection to the database for the given instance
 
-    def create_database_connection(self):
-        """Creates database connection
-
-        If there is not a file at the database location a sqllite database
-          is created at that location.
+        Returns:
+            sqlite3.Connection: A sqlite connection object to enable database actions in
+            other methods.
         """
         try:
-            self._conn = sqlite3.connect(self.__database_location)
+            conn = sqlite3.connect(self.__database_location)
+            return conn
         except sqlite3.OperationalError as e:
             print(f"Failed to open {self.__database_location}")
 
     def create_table(self):
         """Creates table to hold file changes into the event."""
         try:
-            c = self._conn.cursor()
+            c = self.create_database_connection().cursor()
             c.execute(self.__create_table_sql())
         except Error as e:
             print(e)
+        finally:
+            c.close()
 
     # I inadvertently just set the expectation that all time conversions will be done before
     # this function is called. I think this is a good idea. It means that the time conversion
@@ -55,7 +54,7 @@ class FileWatcherDatabase:
         event_type: str,
         event_location: str,
         file_type: str,
-        move_destination: str = None,
+        move_destination: Union[str, None] = None,
     ):
         """
         Inserts data into the database.
@@ -71,67 +70,77 @@ class FileWatcherDatabase:
         Raises:
             sqlite3.Error: If there is an error with the database operation.
         """
-        if self.__conn is None:
-            self.__conn = sqlite3.connect(self.__database_location)
         try:
-            c = self.__conn.cursor()
+            conn = self.create_database_connection()
+            c = conn.cursor()
             c.execute(
                 self.__insert_data_sql(),
                 (event_time, event_type, event_location, file_type, move_destination),
             )
-            self.__conn.commit()
+            conn.commit()
         except sqlite3.Error as e:
             print(f"Database error: {e}")
+        finally:
+            conn.close()
 
     def query_by_file_extension(self, ext_param: str) -> list:
-        """Queries the database by file type.
+        """Query the data base for changes that have to files with a given extension.
 
         Args:
             ext_param (str): The file extension of interest.
+
+        Returns:
+            list: Each item in the list is a database record matching the query condition
         """
-        if self.__conn is None:
-            self.__conn = sqlite3.connect(self.__database_location)
 
-        c = self.__conn.cursor()
-        c.execute(self.__file_extension_query(), (ext_param,))
-
-        rows = c.fetchall()
-        return rows
+        conn = self.create_database_connection()
+        try:
+            c = conn.cursor()
+            c.execute(self.__file_extension_query(), (ext_param,))
+            rows = c.fetchall()
+            return rows
+        except sqlite3.Error as e:
+            print(f"Query Error: {e}")
+        finally:
+            conn.close()
 
     def query_by_event_type(self, event_type: str) -> list:
         """Queries the database by event type.
 
         Args:
             event_type (str): The event type of interest.
+        Returns:
+            list: Each item in the list is a database record matching the query condition
         """
-        if self.__conn is None:
-            self.__conn = sqlite3.connect(self.__database_location)
-
+        conn = self.create_database_connection()
         try:
-            c = self.__conn.cursor()
+            c = conn.cursor()
             c.execute(self.__event_type_query(), (event_type,))
+            rows = c.fetchall()
+            return rows
         except sqlite3.Error as e:
             print(f"Database error: {e}")
-
-        rows = c.fetchall()
-
-        return rows
+        finally:
+            conn.close()
 
     def query_by_event_location(self, event_location: str) -> list:
         """Queries the database by event location.
 
         Args:
             event_location (str): The event location of interest.
+        Returns:
+            list: Each item in the list is a database record matching the query condition
         """
-        if self.__conn is None:
-            self.__conn = sqlite3.connect(self.__database_location)
-
-        c = self.__conn.cursor()
-        c.execute(self.__event_location_query(), (f"{event_location}%",))
-
-        rows = c.fetchall()
-
-        return rows
+        conn = self.create_database_connection()
+        try:
+            c = conn.cursor()
+            c.execute(self.__event_location_query(), (event_location,))
+            rows = c.fetchall()
+            return rows
+        except sqlite3.Error as e:
+            print(f"Database error: {e}")
+        finally:
+            conn.close()
 
     def query_by_event_date(self, start_time: float, end_time: float) -> list:
         """Queries the database by event date.
@@ -141,21 +150,25 @@ class FileWatcherDatabase:
                 (e.g., epoch time).
             end_time (int): The end time of interest. Should be in integer form
                 (e.g., epoch time).
+        Returns:
+            list: Each item in the list is a database record matching the query condition
         """
-        if self.__conn is None:
-            self.__conn = sqlite3.connect(self.__database_location)
-        c = self.__conn.cursor()
-        c.execute(
-            self.__event_date_query(),
-            (
-                start_time,
-                end_time,
-            ),
-        )
-
-        rows = c.fetchall()
-
-        return rows
+        conn = self.create_database_connection()
+        try:
+            c = conn.cursor()
+            c.execute(
+                self.__event_date_query(),
+                (
+                    start_time,
+                    end_time,
+                ),
+            )
+            rows = c.fetchall()
+            return rows
+        except sqlite3.Error as e:
+            print(f"Database error: {e}")
+        finally:
+            conn.close()
 
     # Queries are defined below. All queries are private methods that return the SQL as
     # a string. THe SQL must be parameterized.
